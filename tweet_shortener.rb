8bffc1683c45a883f90175d3f32cda45bc14ec45
w.h.sinn <w.h.sinn@gmail.com>
2019-03-26 15:34:06 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def dictionary
+  "hello" becomes 'hi'
+"to, two, too" become '2' 
+"for, four" become '4'
+'be' becomes 'b'
+'you' becomes 'u'
+"at" becomes "@" 
+"and" becomes "&"
+end
+
+def word_substituter(tweet_string)
+  
+end
+
+
+def bulk_tweet_shortener
+end
